adc5e565fbba99a38856280f01820b7c453ef8e3
Nate Matthews <natezmatthews@gmail.com>
2021-06-27 17:37:06 -0400

    Add line chart

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import LineChart from '../components/LineChart';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
@@ -17,6 +18,7 @@ const Tab1: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
+        <LineChart />
       </IonContent>
     </IonPage>
   );
